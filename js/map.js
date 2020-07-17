846cfbe543d26ec0102b5ef21557b8aeaa68d43a
starkhub <mainjh83@gmail.com>
2020-07-17 22:06:47 +0200

    Maps OK

--- js/map.js
+++ js/map.js
@@ -2,25 +2,29 @@
       // prompted by your browser. If you see the error "The Geolocation service
       // failed.", it means you probably did not give permission for the browser to
       // locate you.
-      var map, infoWindow;
+      var api_key = config.secret_key;
+      var map, infoWindow, marker;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
-          zoom: 6
+          zoom: 15
         });
         infoWindow = new google.maps.InfoWindow;
-
+        
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
+
           navigator.geolocation.getCurrentPosition(function(position) {
             var pos = {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-
+            marker = new google.maps.Marker({position: pos, map: map});
+/*
             infoWindow.setPosition(pos);
             infoWindow.setContent('Location found.');
             infoWindow.open(map);
+*/
             map.setCenter(pos);
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
@@ -34,8 +38,15 @@
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
+                              'Erreur : nous ne pouvons pas vous localiser.' :
+                              'Erreur : votre navigateur ne supporte pas la géolocalisation.');
         infoWindow.open(map);
       }
-      
+
+      function loadjs() {
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "https://maps.googleapis.com/maps/api/js?key=" + api_key + "&maptype=roadmap&callback=initMap";
+        document.body.appendChild(script);
+     }
+     window.onload = loadjs();
